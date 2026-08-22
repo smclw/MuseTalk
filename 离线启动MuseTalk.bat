340f519426e5a0b02c023d8a5ced3e38b75ceb55
smclw <153088025+smclw@users.noreply.github.com>
2026-08-22 23:06:11 +0800

    Add files via upload

--- 离线启动MuseTalk.bat
+++ 离线启动MuseTalk.bat
@@ -10,12 +10,12 @@ set "TRANSFORMERS_OFFLINE=1"
 echo ============================================================
 echo MuseTalk 1.5
 echo NVIDIA GTX 1650 / FP16
-echo 本地离线模式
+echo Local Offline Mode
 echo ============================================================
 echo.
 
 ".venv\Scripts\python.exe" app.py --use_float16
 
 echo.
-echo MuseTalk 已停止。
+echo MuseTalk has stop。
 pause
